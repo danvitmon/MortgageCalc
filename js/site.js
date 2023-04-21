6af7d9e99fc1bf0b91653e77a1550d73a893ffc1
Daniel Monastirsky <danvitmon@gmail.com>
2023-04-21 13:56:59 -0700

    mostly done

--- js/site.js
+++ js/site.js
@@ -1,172 +1,84 @@
-function add() {
-  // get the numbers
-  let numberOne = document.getElementById("firstNumber").value;
-  let numberTwo = document.getElementById("secondNumber").value;
+function calcMortgage(){
+  let amount = parseFloat(document.getElementById("amount").value);
+  let months = parseFloat(document.getElementById("months").value);
+  let rate = parseFloat(document.getElementById("rate").value);
 
-  // turn them into integers
-  numberOne = parseFloat(numberOne);
-  numberTwo = parseFloat(numberTwo);
-
-  // add the first and second numbers
-  let sum = numberOne + numberTwo;
-
-  // display the result
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = sum;
-}
-
-function subtract() {
-  let numberOne = document.getElementById("firstNumber").value;
-  let numberTwo = document.getElementById("secondNumber").value;
-
-  numberOne = parseFloat(numberOne);
-  numberTwo = parseFloat(numberTwo);
-
-  let sum = numberOne - numberTwo;
-
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = sum;
-}
-
-function multiply() {
-  let numberOne = document.getElementById("firstNumber").value;
-  let numberTwo = document.getElementById("secondNumber").value;
-
-  numberOne = parseFloat(numberOne);
-  numberTwo = parseFloat(numberTwo);
-
-  let sum = numberOne * numberTwo;
-
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = sum;
-}
-
-function divide() {
-  let numberOne = document.getElementById("firstNumber").value;
-  let numberTwo = document.getElementById("secondNumber").value;
-
-  numberOne = parseFloat(numberOne);
-  numberTwo = parseFloat(numberTwo);
-
-  let sum = numberOne / numberTwo;
-
-  if (numberTwo == 0) {
-    sum = "Cannot divide by zero";
+  if(isNaN(amount) || amount <= 0){
+      Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Please enter a positive dollar value for the loan amount",
+          backdrop: false,
+      });
+      return;
+  }
+  if(!Number.isInteger(months) || months < 1){
+      Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Please enter a positive number of months for the term",
+          backdrop: false,
+      });
+      return;
+  }
+  if (isNaN(rate) || rate < 0) {
+      Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Please enter a positive percentage value for the interest rate",
+          backdrop: false,
+      });
+      return;
   }
 
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = sum;
+  let monthlyPay = (amount * (rate/1200)) / (1 - Math.pow((1 + (rate/1200)), -months)) 
+  let totalInt = buildTable(amount, months, rate, monthlyPay);
+  displayTotals(monthlyPay, amount, totalInt);
 }
 
-function sumAll() {
-  let numberString = document.getElementById("numberSeries").value;
-  // numberString = '12345'
+function buildTable(amount, months, rate, monthlyPay){
+  table = document.getElementById("calcTable");
+  template = document.getElementById("trtemp");
 
-  let numberArr = numberString.split("");
-  // numberArr = ['1', '2', '3', '4', '5']
+  let totalInt = 0;
+  let balance = amount;
 
-  let sum = 0; // running total
+  for(i = 1; i <= months; i++){
+      let tableRow = document.importNode(template.content, true);
 
-  for (let i = 0; i < numberArr.length; i = i + 1) {
-    let currentNumber = numberArr[i];
-    // currentNumber = '1'
+      tableRow.querySelector("[data-id='month']").textContent = i;
 
-    currentNumber = parseInt(currentNumber);
-    // currentNumber = 1
+      tableRow.querySelector("[data-id='pay']").textContent = formatCurrency(monthlyPay);
 
-    sum = sum + currentNumber;
+      let interest = balance * (rate / 1200);
+      tableRow.querySelector("[data-id='int']").textContent = formatCurrency(interest);
+
+      let principal = monthlyPay - interest;
+      tableRow.querySelector("[data-id='prin']").textContent = formatCurrency(principal);
+
+      totalInt += interest;
+      tableRow.querySelector("[data-id='total']").textContent = formatCurrency(totalInt);
+
+      balance -= principal;
+      tableRow.querySelector("[data-id='bal']").textContent = formatCurrency(Math.abs(balance));
+
+      table.appendChild(tableRow);
   }
 
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = sum;
+  return totalInt;
 }
 
-function multiplyAll() {
-
-    let numberString = document.getElementById("numberSeries").value;
-  
-    let numberArr = numberString.split("");
-  
-    let sum = 1;
-  
-    for (let i = 0; i < numberArr.length; i = i + 1) {
-      let currentNumber = numberArr[i];
-  
-      currentNumber = parseInt(currentNumber);
-  
-      sum = sum * currentNumber;
-    }
-  
-    let resultsDiv = document.getElementById("results");
-    resultsDiv.innerText = sum;
+function displayTotals(monthlyPay, amount, totalInt){
+  let cost = amount + totalInt
+  document.getElementById("monthly").textContent = formatCurrency(monthlyPay);
+  document.getElementById("tprin").textContent = formatCurrency(amount);
+  document.getElementById("tint").textContent = formatCurrency(totalInt);
+  document.getElementById("tcost").textContent = formatCurrency(cost);
 }
 
-function minimum() {
-  let numberString = document.getElementById("numberSeries").value;
-
-  let numberArr = numberString.split("");
-
-  let min = parseInt(numberArr[0]);
-
-  for (let i = 0; i < numberArr.length; i = i + 1) {
-    let currentNumber = numberArr[i];
-    
-    currentNumber = parseInt(currentNumber);
-
-    if(currentNumber < min) {
-      max = currentNumber;
-    }
-
-  }
-
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = min;
-}
-
-function maximum() {
-
-    let numberString = document.getElementById("numberSeries").value;
-
-    let numberArr = numberString.split("");
-  
-    let max = parseInt(numberArr[0]);
-  
-    for (let i = 0; i < numberArr.length; i = i + 1) {
-      let currentNumber = numberArr[i];
-      
-      currentNumber = parseInt(currentNumber);
-
-      if(currentNumber > max) {
-        max = currentNumber;
-      }
-  
-    }
-  
-    let resultsDiv = document.getElementById("results");
-    resultsDiv.innerText = max;
- // check each number and see if its larger than
- // the largest number ive found so far
-}
-
-function average() {
-    // get the sum of all
-    // divide the sum by the amount of numbers
-    // display it
-  let numberString = document.getElementById("numberSeries").value;
-
-  let numberArr = numberString.split("");
-
-  let sum = 0;
-
-  for (let i = 0; i < numberArr.length; i = i + 1) {
-    let currentNumber = numberArr[i];
-
-    currentNumber = parseInt(currentNumber);
-
-    sum = sum + currentNumber;
-    avg = sum / numberArr.length;
-  }
-
-  let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = avg;
+function formatCurrency(num){
+  return num.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+  });
 }
